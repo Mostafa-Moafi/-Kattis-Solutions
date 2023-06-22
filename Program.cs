ce33661d54e10e61f0d659921b7623705bd621f8
Mostafa Moafi <moafimostafa80@gmail.com>
2023-06-22 16:45:00 +0330

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace kattis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi");
+        }
+    }
+}
