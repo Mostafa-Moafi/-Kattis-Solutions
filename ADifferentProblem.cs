a961dceea2b48c48601dc1b6d983b8fad8f93663
Mostafa Moafi <moafimostafa80@gmail.com>
2024-06-18 22:47:59 +0330

    Solve "A Different Problem" Problem

---
+++ ADifferentProblem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace kattis
+{
+    internal class ADifferentProblem
+    {
+        static void Main()
+        {
+            string line;
+            var list = new List<long>();
+            while (!string.IsNullOrEmpty(line = Console.ReadLine()))
+            {
+                var nums = line.Split(' ');
+                var a = Int64.Parse(nums[0]);
+                var b = Int64.Parse(nums[1]);
+
+                list.Add(Math.Abs(a - b));
+            }
+
+            foreach (var i in list)
+                Console.WriteLine(i);
+        }
+    }
+}
