083b7d82fde5e0dc587e19cd686564f1eb94fed0
Mostafa Moafi <moafimostafa80@gmail.com>
2024-06-18 23:20:27 +0330

    Solve "99 Problems" Problem

---
+++ 99Problems.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace kattis
+{
+    internal class _99Problems
+    {
+        static void Main(string[] args)
+        {
+            var input = int.Parse(Console.ReadLine());
+            if (input <= 99)
+                Console.WriteLine(99);
+            else
+            {
+                var remain = input % 100;
+                if (remain == 0)
+                    Console.WriteLine(input - 1);
+                else
+                {
+                    if (remain >= 49)
+                        Console.WriteLine(input + (99 - remain));
+                    else
+                        Console.WriteLine(input - (remain + 1));
+                }
+            }
+        }
+    }
+}
